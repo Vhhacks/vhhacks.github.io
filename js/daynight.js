64b2767aa5362f8e26630310df23b9d3a1dc36dd
andy <andyshenproper@gmail.com>
2019-09-11 21:57:50 -0700

    Change back to png

--- js/daynight.js
+++ js/daynight.js
@@ -1,7 +1,7 @@
 var current= new Date()
 var day_night=current.getHours()
 if (day_night>=8 && day_night <= 16){
-    document.getElementsByClassName("header-area")[0].setAttribute('style', "background: #0e2946 url('images/landingpage skyline.svg') no-repeat scroll center bottom / cover");
+    document.getElementsByClassName("header-area")[0].setAttribute('style', "background: #0e2946 url('images/vhhacksbackgroundday.png') no-repeat scroll center bottom / cover");
 } else{
-    document.getElementsByClassName("header-area")[0].setAttribute('style', 'background: #0e2946 url(\'images/landingpage skyline dusk.svg\') no-repeat scroll center bottom / cover');
+    document.getElementsByClassName("header-area")[0].setAttribute('style', 'background: #0e2946 url(\'images/vhhacksbackgrounddusk.png\') no-repeat scroll center bottom / cover');
 }
